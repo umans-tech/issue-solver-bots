6c08dc132f4db8a3f26e42c415dd9f5f0d6e36c0
wassel alazhar <wassel.alazhar@gmail.com>
2024-10-07 12:32:16 +0200

    refactor(#8): init issue-solver python project with uv 🐍✨

---
+++ issue-solver/README.md
@@ -0,0 +1,5 @@
+# ISSUE-SOLVER
+
+This project is a collection of bots that help solve issues and automate tasks in software development workflows.
+The bots are designed to work with popular platforms like GitLab and GitHub, and can be extended to support other
+platforms as well.
---
+++ issue-solver/src/issue_solver/__init__.py
@@ -0,0 +1,15 @@
+import os
+from typing import Callable
+
+from issue_solver.start_resolution import SolveIssueCommand, AgentModel, AgentName, IssueDescription
+
+
+def main() -> None:
+    print("Hello from issue-solver!")
+
+def prepare_solve_issue(generate_patch: Callable[[SolveIssueCommand], None]) -> Callable[[], None]:
+    return lambda: generate_patch(SolveIssueCommand(
+        agent_model=AgentModel(os.environ["AGENT_MODEL"]),
+        agent_name=AgentName(os.environ["AGENT_NAME"]),
+        issue_description=IssueDescription(os.environ["ISSUE_DESCRIPTION"]),
+    ))
