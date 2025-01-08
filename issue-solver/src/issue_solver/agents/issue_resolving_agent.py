abb6532bc06533bbf98e1d253900c86b7eee1d00
wassel alazhar <wassel.alazhar@gmail.com>
2025-01-08 01:45:29 +0100

    refactor(#8): refactor app settings in main ✨

--- issue-solver/src/issue_solver/agents/issue_resolving_agent.py
+++ issue-solver/src/issue_solver/agents/issue_resolving_agent.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
 
+from issue_solver.models.model_settings import ModelSettings
 from issue_solver.models.supported_models import SupportedAIModel
 
 
@@ -9,9 +10,26 @@ class IssueDescription(str):
     pass
 
 
+@dataclass(frozen=True)
+class VersionedAIModel:
+    ai_model: SupportedAIModel
+    version: str | None = None
+
+    def __repr__(self):
+        if self.version:
+            return f"{self.ai_model.value}-{self.version}"
+        return self.ai_model.value
+
+
+@dataclass(frozen=True)
+class VersionedAIModelWithSettings:
+    model: VersionedAIModel
+    settings: ModelSettings
+
+
 @dataclass(frozen=True, kw_only=True)
 class ResolveIssueCommand:
-    model: SupportedAIModel
+    model: VersionedAIModel
     issue_description: IssueDescription
     repo_path: Path
 
--- issue-solver/src/issue_solver/agents/supported_agents.py
+++ issue-solver/src/issue_solver/agents/supported_agents.py
@@ -2,6 +2,7 @@ from enum import StrEnum
 from typing import Self
 
 from issue_solver.agents.issue_resolving_agent import IssueResolvingAgent
+from issue_solver.models.model_settings import ModelSettings
 
 
 class SupportedAgent(StrEnum):
@@ -11,5 +12,7 @@ class SupportedAgent(StrEnum):
     ANTHROPIC_TOOLS = "anthropic-tools"
 
     @classmethod
-    def get(cls, agent: Self) -> IssueResolvingAgent:
+    def get(
+        cls, agent: Self, *ai_models_settings: ModelSettings
+    ) -> IssueResolvingAgent:
         raise NotImplementedError(" SupportedAgent.get is not implemented yet.")
