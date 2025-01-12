4fa1f6ec816d678fa92a1ddddbf7d53803383b37
wassel alazhar <wassel.alazhar@gmail.com>
2025-01-12 23:43:47 +0100

    feat(#8): github issue tracker ✨

--- issue-solver/src/issue_solver/issues/trackers/github_issue_tracker.py
+++ issue-solver/src/issue_solver/issues/trackers/github_issue_tracker.py
@@ -1,11 +1,19 @@
 from enum import StrEnum
-from typing import Literal
+from http import HTTPStatus
+from pathlib import Path
+from typing import Literal, Self
 
+import requests
 from pydantic import AnyUrl, Field
 from pydantic_core import Url
 from pydantic_settings import SettingsConfigDict
 
-from issue_solver.issues.issue import IssueReference, IssueInfo
+from issue_solver.issues.issue import (
+    IssueReference,
+    IssueInfo,
+    IssueInternalId,
+    IssueId,
+)
 from issue_solver.issues.trackers.issue_tracker import (
     IssueTracker,
 )
@@ -17,11 +25,74 @@ class GithubObjectType(StrEnum):
     MR = "MR"
 
 
+class PullRequestInternalId(IssueInternalId):
+    type: Literal["PR"] = "PR"
+
+
 class GithubIssueTracker(IssueTracker):
+    def __init__(self, settings: "GithubIssueTracker.Settings") -> None:
+        self.settings = settings
+
+    @classmethod
+    def of(cls, settings: "GithubIssueTracker.Settings") -> Self:
+        return cls(settings=settings)
+
     def describe_issue(self, issue_reference: IssueReference) -> IssueInfo | None:
-        raise NotImplementedError(
-            "GithubIssueTracker.get_issue_description is not implemented"
+        issue_path = self.get_issue_path(issue_reference)
+        headers = {
+            "Accept": f"application/vnd.github.{self.settings.api_version}+json",
+            "Authorization": f"Bearer {self.
+            settings.private_token}"
+            if self.settings.private_token
+            else None,
+        }
+        try:
+            response = requests.get(
+                f"{self.settings.base_url}{issue_path}", headers=headers
+            )
+            if response.status_code == HTTPStatus.NOT_FOUND:
+                return None
+            if response.status_code in {HTTPStatus.UNAUTHORIZED, HTTPStatus.FORBIDDEN}:
+                raise self.permission_error(response, issue_path, response.status_code)
+            if not response.ok:
+                raise RuntimeError(
+                    f"Failed to get issue from {self.settings.base_url}{issue_path} with status code {response.status_code}",
+                    response.text,
+                )
+
+            data = response.json()
+            title = data.get("title", "")
+            body = data.get("body", "")
+            return IssueInfo(title=title, description=body)
+        except requests.exceptions.RequestException as e:
+            raise RuntimeError(
+                f"Failed to get issue from {self.settings.base_url}{issue_path}",
+                e,
+            )
+
+    def permission_error(
+        self, response, get_issue_path: str, response_code: int | None = None
+    ) -> PermissionError:
+        return PermissionError(
+            f"Unauthorized call to {self.settings.base_url}{get_issue_path} with status code {response_code}",
+            response.text,
         )
+
+    @staticmethod
+    def get_issue_path(issue_reference: IssueReference) -> str:
+        match issue_reference:
+            case PullRequestInternalId():
+                owner_repo = issue_reference.project_id
+                return f"repos/{owner_repo}/pulls/{issue_reference.iid}"
+            case IssueInternalId():
+                owner_repo = issue_reference.project_id
+                return f"repos/{owner_repo}/issues/{issue_reference.iid}"
+            case IssueId():
+                raise RuntimeError("GitHub does not support global issue IDs")
+            case Path():
+                return f"repos/{issue_reference}"
+            case _:
+                raise ValueError(f"Unsupported reference: {issue_reference}")
 
     class Settings(ApiBasedIssueTrackerSettings):
         type: Literal["GITHUB"] = "GITHUB"
