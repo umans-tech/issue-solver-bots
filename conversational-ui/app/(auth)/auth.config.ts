09f86176034690ab2add58d2238d97533bd458ba
wassel alazhar <wassel.alazhar@gmail.com>
2025-02-27 01:47:10 +0100

    build(terraform): debug auth 🔐

--- conversational-ui/app/(auth)/auth.config.ts
+++ conversational-ui/app/(auth)/auth.config.ts
@@ -9,6 +9,8 @@ export const authConfig = {
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
   ],
+  secret: process.env.AUTH_SECRET,
+  trustHost: true,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
