342a7f32c4d91ed51dfc8d205cbfae5cc761a828
wassel alazhar <wassel.alazhar@gmail.com>
2025-02-28 17:12:46 +0100

    build(terraform): debug auth 🔐

--- conversational-ui/lib/db/queries.ts
+++ conversational-ui/lib/db/queries.ts
@@ -30,7 +30,7 @@ export async function getUser(email: string): Promise<Array<User>> {
   try {
     return await db.select().from(user).where(eq(user.email, email));
   } catch (error) {
-    console.error('Failed to get user from database');
+    console.error('Failed to get user from database', error);
     throw error;
   }
 }
