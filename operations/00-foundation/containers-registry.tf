4363ac521e3fdef633590b64f410d7695557d76a
wassel alazhar <wassel.alazhar@gmail.com>
2025-03-13 12:12:46 +0100

    feat(operations): initialize AWS infrastructure with ECR 🐳🛠️

---
+++ operations/00-foundation/containers-registry.tf
@@ -0,0 +1,53 @@
+# Create an Elastic Container Registry repository
+resource "aws_ecr_repository" "app_repository" {
+  name                 = "umans-platform"
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
+  tags = {
+    Name        = "umans-platform"
+    Environment = "foundation"
+    Terraform   = "true"
+  }
+}
+
+# Lifecycle policy to manage image retention
+resource "aws_ecr_lifecycle_policy" "app_lifecycle_policy" {
+  repository = aws_ecr_repository.app_repository.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Retain production tagged images"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = [
+            "umans-platform-webapi-production",
+            "umans-platform-worker-production"
+          ]
+          countType     = "imageCountMoreThan"
+          countNumber   = 6
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus     = "any"
+          countType     = "imageCountMoreThan"
+          countNumber   = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
