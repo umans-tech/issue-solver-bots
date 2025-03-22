1fa6d55ab91ecc9976a26ebf261f64a25e7f0a1d
alazharn <alazhar.naji@gmail.com>
2025-03-22 15:24:32 +0000

    fix(ui): fix ui errors, simplify CodeBlock component structure and enhance paragraph rendering in Markdown; remove unused navigation type reference in TypeScript definitions 🛠️✨

--- conversational-ui/components/code-block.tsx
+++ conversational-ui/components/code-block.tsx
@@ -16,14 +16,12 @@ export function CodeBlock({
 }: CodeBlockProps) {
   if (!inline) {
     return (
-      <div className="not-prose flex flex-col">
-        <pre
-          {...props}
-          className={`text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
-        >
-          <code className="whitespace-pre-wrap break-words">{children}</code>
-        </pre>
-      </div>
+      <code
+        className={`text-sm w-full block overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900 whitespace-pre-wrap break-words`}
+        {...props}
+      >
+        {children}
+      </code>
     );
   } else {
     return (
