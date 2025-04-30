a207798a1d1bf7cc0bc0f7814737d073941db923
wassel alazhar <wassel.alazhar@gmail.com>
2025-04-30 17:45:33 +0200

    feat(ui): bootstrap a landing page and allow access to landing page in authentication middleware 🛬📄✨

--- conversational-ui/middleware.ts
+++ conversational-ui/middleware.ts
@@ -5,6 +5,6 @@ import { authConfig } from '@/app/(auth)/auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  // Matcher ignoring /terms and /privacy pages to make them publicly accessible
-  matcher: ['/((?!terms|privacy).)*', '/api/:path*', '/login', '/register'],
+  // Matcher ignoring /terms, /privacy, and /landing pages to make them publicly accessible
+  matcher: ['/((?!terms|privacy|landing).)*', '/api/:path*', '/login', '/register'],
 };
