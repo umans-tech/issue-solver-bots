316aa67ed6481bc87ba976c985bce2ba082ecd1c
wassel alazhar <wassel.alazhar@gmail.com>
2025-05-16 15:54:39 +0200

    feat(conversational-ui): containerize conversational-ui and deploy it in AWS App runner 🐳🚀✨
    feat(conversational-ui): remove NEXTAUTH_URL_INTERNAL variable for cleaner configuration
    
    feat(conversational-ui): remove DB_MIGRATE environment variable and update justfile for cleaner Docker run
    
    feat(conversational-ui): set HOSTNAME environment variable to '0.0.0.0' for improved accessibility
    
    feat(next-config): set hostname to '0.0.0.0' for improved accessibility
    
    feat(docker): update HOST and HOSTNAME environment variables for consistency
    
    feat(conversational-ui): update health check configuration parameters for improved reliability
    
    Revert "feat(docker): simplify Dockerfile by removing unnecessary user creation and permissions"
    
    This reverts commit 7136394055bae0033ccc389d08ca81769f632632.
    
    feat(docker): simplify Dockerfile by removing unnecessary user creation and permissions
    
    feat(docker): add .dockerignore and reorganize environment variables in Dockerfile
    
    feat(conversational-ui): update health check protocol to HTTP and simplify Dockerfile command
    
    feat(conversational-ui): add VPC connector and update health check to use TCP protocol
    
    feat(conversational-ui): update health check to use HTTP protocol and remove VPC connector configuration + simplify ✂️
    
    feat(conversational-ui): update health check to use TCP protocol and adjust Dockerfile settings
    
    feat(conversational-ui): add health check configuration and update middleware for health endpoint
    
    feat(conversational-ui): remove health check configuration from Terraform
    
    feat(dockerfile): update command to allow dynamic port configuration
    
    feat(conversational-ui): add health check configuration for service monitoring
    
    feat(dockerfile): update app runner settings and simplify entrypoint
    
    feat(docker-entrypoint): allow Next.js to listen on all network interfaces
    
    feat(cd-workflow): add EXA_API_KEY build argument for Docker
    
    feat(cd-workflow): add OPENAI_API_KEY build argument for Docker
    
    feat(cd-workflow): update Docker build arguments for Postgres and Redis URLs
    
    feat(docker): add build arguments for Postgres and Redis URLs in Dockerfile
    
    feat(conversational-ui): add App Runner configuration and Docker build for conversational UI 🏃✨
    
    feat(conversational-ui): set output to standalone in Next.js configuration
    
    feat(docker): add Docker support with entrypoint script and build configurations

--- conversational-ui/next.config.js
+++ conversational-ui/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  output: 'standalone',
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
