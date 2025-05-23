8932e2aa886cc4378677959cb7a10a4769a9a8a6
wassel alazhar <wassel.alazhar@gmail.com>
2025-05-23 03:02:39 +0200

    refactor(conversational-ui): drop Vercel 👋

--- operations/02-deploy/main.tf
+++ operations/02-deploy/main.tf
@@ -27,10 +27,8 @@ data "aws_caller_identity" "current" {}
 locals {
   environment_name               = terraform.workspace
   deployment_target              = terraform.workspace == "production" ? "production" : "preview"
-  vercel_deployment_target       = "production"
   environment_name_suffix        = terraform.workspace == "production" ? "" : "-${local.environment_name}"
   domain_prefix                 = terraform.workspace == "production" ? "" : "${local.environment_name}."
-  conversational_ui_project_name = "conversational-ui${local.environment_name_suffix}"
   landing_domain                 = "${local.domain_prefix}umans.ai"
   app_domain                     = "app.${local.domain_prefix}umans.ai"
   api_domain                     = "api.${local.domain_prefix}umans.ai"
--- operations/02-deploy/outputs.tf
+++ operations/02-deploy/outputs.tf
@@ -1,17 +1,7 @@
-output "vercel_conversational_ui_url" {
-  description = "The default domain for the conversational UI app"
-  value       = "https://${vercel_deployment.conversational_ui.url}"
-}
-
 # Output App Runner URL
 output "conversational_ui_url" {
   description = "The URL of the App Runner service for the Conversational UI"
   value = "https://${aws_apprunner_service.conversational_ui.service_url}"
-}
-
-output "conversational_ui_app_id" {
-  description = "The ID of the App for the Conversational UI"
-  value       = vercel_project.conversational_ui.id
 }
 
 output "process_queue_url" {
