4157a3cda61bdf7070f125ac4094fd6e55fcf7dd
wassel alazhar <wassel.alazhar@gmail.com>
2025-09-24 07:01:13 +0200

    feat(worker, terraform): enable S3 knowledge blob integration for worker Lambda ✨📦
    • Removed redundant log statements from worker message processing
    • Added IAM policy and role attachment in Terraform for S3 bucket access by worker Lambda

--- operations/02-deploy/iam.tf
+++ operations/02-deploy/iam.tf
@@ -21,6 +21,42 @@ resource "aws_iam_policy" "lambda_vpc_access" {
   })
 }
 
+# IAM policy to allow worker Lambda to access the knowledge blob S3 bucket
+resource "aws_iam_policy" "worker_blob_s3_access" {
+  name        = "worker-blob-s3-access${local.environment_name_suffix}"
+  description = "Allow worker Lambda to read/write in knowledge blob S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ListBucket"
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::${data.terraform_remote_state.provision.outputs.blob_bucket_name}"
+      },
+      {
+        Sid    = "ObjectRW"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::${data.terraform_remote_state.provision.outputs.blob_bucket_name}/*"
+      }
+    ]
+  })
+}
+
+# Attach S3 access policy to worker execution role
+resource "aws_iam_role_policy_attachment" "worker_blob_s3_access" {
+  role       = aws_iam_role.worker_lambda_exec.name
+  policy_arn = aws_iam_policy.worker_blob_s3_access.arn
+}
+
 # Attach policy to webapi execution role
 resource "aws_iam_role_policy_attachment" "webapi_vpc_access" {
   role       = aws_iam_role.webapi_lambda_exec.name
